e97b0d933eca7f7c184221a436023a285a6277cf
Melaati Jayah <fathimajayah876@gmail.com>
2019-01-16 13:18:23 -0600

    grraded class work incomlete

---
+++ problem3.js
@@ -0,0 +1,38 @@
+// ### Problem 3:
+// Put the start of your program in a main function.
+// Create an attendance array. Ask the user if a teacher, student, or parent is checking in.
+// Use a switch statement to add the teacher/student/parent to the attendance array, then log the option they chose.
+//     Repeat this five times.
+
+
+main();
+function main()
+{
+
+
+var attendanceArray = ["teacher", "student","parent"];
+    var checkin = prompt("are you a teacher, student , or a parent checking in?");
+    switch(checkin)
+{
+    case "teacher":
+        console.log("teacher");
+        break;
+    case "student":
+        console.log("student");
+        break;
+    case "parent":
+        console.log("parent");
+        break;
+    default:
+       var result =  prompt("are you a teacher, student, or parent checking in?");
+
+
+       attendanceArray.forEach(answer);
+    function answer
+    [
+    ]
+}
+
+}
+
+
